72dd9d3dbcc5c4b0dd482709434a8beb7fea8019
Altafs01 <altafshaikh1697@gmail.com>
2023-08-27 12:49:15 +0530

    javascript y

--- function.js
+++ function.js
@@ -4,7 +4,7 @@
 
 //     }
 //     else {
-//         return confirm (`do you have permission from your parent`);
+//         return confirm (`do you have permission from your parent?`);
 //     }
 // }
 
@@ -26,9 +26,11 @@
 // function sub(x,y) {
 //     return x - y;
 // }
+
 // function mul(c,d) {
 //     return c * d;
 // }
+
 // function div(c,d) {
 //     return c / d;
 // }
@@ -54,20 +56,20 @@
 
 
 // let multiplictaionResult = mul(firstNumber, secondNumber);
-// alert ( `The result of multiplication is `+  multiplictaionResult )
+// alert ( `The result of multiplication is `+  multiplictaionResult );
 // // alert   ("The result of multiplication is "+ multiplictaionResult);
 
 
 // let divisionResult = div(firstNumber, secondNumber);
 // alert   ("The result of division is "+ divisionResult);
 
+
+// ------------------------------------------------------------------------
 // function showMessage() {
 //     let message = "hello, I'm Anas!";
 //     alert( message)
 // }
 
-// let hello = "Hi";
-// console.log(hello)
 
 // let greeter = "hey hi";
 // let hello = "Hi"
@@ -78,7 +80,7 @@
 //     console.log(hello);
 // }
 // console.log(hello);
-
+// --------------------------------------------
 // var greeter = "hey hi";
 // var times = 4;
 
@@ -95,29 +97,75 @@
 // showMessage();
 // console.log(greeter);
 
+// ------------------------------------------------
+
   // outer variables-----------------------------
 
 // let username = `Altaf`;
+// var username2 = `Patel`;
 
 // function showMessage(){
 //     let username1 = `Hello `+ username;
 //     alert(username1);
+//     // console.log(username2);
 // }
 
-// showMessage();  
+// showMessage(); 
+// console.log(username) 
 // ------------------------------------------------
 
-let username = `Danish`;
+// let username = `Danish`;
 
-function showMessage(){
-    username = `Altaf`;   //changed the outer variable
-    let message = `Hello `+ username;
-    alert(message);
-    // console.log(message); 
-}
+// function showMessage() {
+//     username = `Altaf`;   //changed the outer variable
+//     let message = `Hello `+ username;
+//     // alert(message);
+//     console.log(message); 
+// }
 
-alert(username); //Danish before the function call
-showMessage();
-alert(username);  //Altaf the value was modified by the function.
+// alert(username); //Danish before the function call
+// showMessage();
+// alert(username);  //Altaf the value was modified by the function.
+// ------------------------------------------------------------------------
+
+// function study(){
+//   console.log("do study");
+//   console.log("do hardwork");
+//   alert("get success");
+// }
+
+// study();
+// study();
+
+// var arr = [12,13,14,15,55];
+
+// console.log(arr);
+
+// arr.push(8);
+// arr.pop();
+// arr.unshift(23);
+// arr.shift();
+// arr.splice()
+
+
+// alert( "The output is " +  arr[3]);
+
+let a = 20;
+let b = '20';
+b = Number (b);
+console.log(typeof(b));
+
+c = a*b;
+// console.log (a + b);
+
+// console.log(a-b);
+// // console.log(a*b);
+// console.log(a/b);
+// console.log( a*a*b*a);
+
+alert (c) = ("The Result of multiplication is " + c);
+
+
+
 
 
