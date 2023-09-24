731cf51b3e012c67099588042d500a4738c29e5c
Altafs01 <altafshaikh1697@gmail.com>
2023-09-24 16:30:22 +0530

    changes i have made

--- function.js
+++ function.js
@@ -342,7 +342,7 @@
 
 // declaration of for loop or syntax of for loop--------------------------
 // for(initialization; check/test expression; increment decrement/update;){
-    // body consisting of multiple statements
+// body consisting of multiple statements
 // }
 
 
@@ -406,219 +406,144 @@
 // }
 
 // ShowStringProperties(Person);
-// ----------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------
 
+// function sumOfMultipleValue(curLimit){
 
-// -------------------------------------------------------------------------
-    // function isOddEvenNumber(num){
-    //     if (num % 2 ===0){
-    //         console.log (`${num}  is even number`) 
-    //     }
-    //     else {
-    //         console.log (`${num}  is odd number`) 
+//     let sumOfMultipleValue = 0;
 
-    //     }
-    
-    // }
+//     for( let i= 1; i <= curLimit; i++){
+//         if(i % 3 ===0 || i % 5 ===0){
+//             // console.log(i);
+//             sumOfMultipleValue +=i;
+//         }
+//     }
+//     console.log(`sumOfMultipleValue of 3 & 5 upto ${curLimit} digit is:`, sumOfMultipleValue)
+// }
 
-    // // console.log( typeof(num));
-        
+// sumOfMultipleValue(10);
+// ----------------------------------------------------------------------------------
 
-    // isOddEvenNumber(12);
-    // isOddEvenNumber(13);
+// function star(row) {
+//     for (i = 1; i <= row; i++) {
+//         let pattern = '';
 
-// ------------------------------------------------------------------------------
+//         for (j = 0; j < i; i++) {
 
+//             pattern += '*';
 
-// function isValidNum(num){
-//     if(isNaN(num))
-//         console.log(`${num} variable is not a number`)
+//         }
 
-//     else{
-//         console.log(`${num} Variable is a valid number`)
+//         console.log(pattern);
+//     }
+// }
+// star(4);
+// --------------------------------------------------------------------------
+
+// var para = document.querySelector ('#para');
+// para.style.fontSize = `40px`;
+// para.style.color = `green`;
+// para.style.backgroundColor = `black`;
+// ----------------------------------------------------------------
+
+// user = {
+//     fName : "Altaf",
+//     lName : "Shaikh",
+//     role : "Admin",
+//     liginCount : 32,
+//     courseList : [],
+//     buyCourse: function(courseName){
+//         this.courseList.push(courseName);
+//     },
+//     getCourseCount: function (){
+//         return ` ${lName} has enrolled for ${this.courseList.length} courses`;
 //     }
 // }
 
-// // isValidNum('one');
-// // isValidNum(1);
-// isValidNum('45');
-// console.log(typeof('45'));
-// // isValidNum('char');
-// // isValidNum(45.5);
-// ------------------------------------------------------------------------------------------------
+// console.log(user.fName);
+// console.log(user.lName);
+// console.log(user.role);
+// console.log(user.liginCount);
+// console.log(user.courseList);
+// console.log(user.lName);
+// user.buyCourse("c++");
+// user.buyCourse("java");
+// user.buyCourse("React Js");
+// console.log(user.getCourseCount());
+// ------------------------------------------------------------------
 
-// function findLargest(num1,num2){
-//     if(num1>num2){
-//         console.log (`${num1} is largest num1`)
-//     }
-//     else if (num1<num2){
-//         console.log (`${num2}  is largest num2`)
+// context tutorial
 
+// var contextObj = {
+//     invokeThisobj() {
+//         return this;
+//     },
+// };
+
+// console.log("2nd position of", contextObj.invokeThisobj);
+
+// class contextObj2 {
+//     constructor(a) {
+//         this.a = a;
 //     }
-//     else{
-//         console.log(`${num1} is equal to ${num2}`)
+
+
+//     func1() {
+//         return this;
 //     }
 // }
 
-// findLargest(10,20);
-// findLargest(20,20);
-// findLargest(35,20);
+// var inst = new contextObj2(2);
+// console.log(inst.func1());
+// var inst2 = new contextObj2(5);
+// console.log(inst2.func1());
 // ------------------------------------------------------------------------------------------------------
 
-// function findLargest(num1, num2, num3){
-//     if(num1>num2 && num2>num3){
-//         console.log(`${num1} is greater largest than ${num2} ${num3}`)
-//     }
-//     else if(num2>num3){
-//         console.log(`${num2} is largest than ${num3}`)
-//     }
-//     else{
-//         console.log(`${num3} is largest number`)
-//     }
-// }
+// var arr = ["India", "Japan", "USA", "Austrilia"];
+var arr2 = new Array("Mumbai", "Pune", "Banglore", "Aurangabad");
+
+// console.log(arr);
+// console.log("Array Length :", arr.length);
+// console.log(arr2[0]);
+
+// // push and pop in array
+
+arr2.pop();
+console.log(arr2);
+arr2.push();
+arr2.push("Jaipur");
+console.log(arr2);
 
 
+// unshift and shift in array 
 
-// findLargest(35,10,2);
-// findLargest(10, 35,25);
-// findLargest(10,12,35);
+// console.time();
+// arr2.unshift("Indore");
+// console.log("After unshift:", arr2);
+// arr2.shift();
+// console.timeEnd();
 
-// -------------------------------------------------------------------------------------------------
+// // Array.form
 
-// function isTrigleEquilateralIsoscelesScalene(side1,side2,side3){
-//     if((side1==side2) && (side1==side3)){
-//         console.log(`Equilateral Trigle`);
-//     }
+// console.log(Array.from("fauzan falke"));
+// console.log(Array.from([2, 3, 4], (x => x * x)));
 
-//     else if(side1==side2 || side2==side3 || side1==side3){
-//         console.log(`isosceles Trigle`);
-//     }
+// var arr3 = "javaScript";
+// console.log(Array.from(arr3, (x => x + 5)));
 
-//     else{
-//         console.log(`Scalene Trigle`);
-//     }
-// }
+// ----------------------------------------------------------------------------------------------------------
 
-// isTrigleEquilateralIsoscelesScalene(10,10,10);
-// isTrigleEquilateralIsoscelesScalene(3,3,5);
-// isTrigleEquilateralIsoscelesScalene(20,21,22);
-// --------------------------------------------------------------------------------------------
+// var isEven = (no) => {
+//     return no % 2 ===0;
+// };
+// console.log(isEven(4) );
 
-// function checkLearYear(year){
-//     if(((year % 4===0)&&(year % 100 !=0)) || (year % 400 ===0)){
-//         console.log (` ${year} leap year`);
-//     }
-//     else{
-//         console.log(` ${year} not a lear year`)
-//     }
-// }
+// Array.every example
 
-// checkLearYear(2012);
-// checkLearYear(2013);
-// checkLearYear(2014);
-// checkLearYear(2015);
-// checkLearYear(2016);
-// ----------------------------------------------------------------------------------------------
+// var array = [2, 4, 6, 10]
+// var result = array.every(isEven);   
+// console.log("Result " + result);
 
-// function findGrand(name,marks){
-
-//     if (marks >= 90 && marks <= 100)
-//         console.log(`${name} you have received S Grade`)
-    
-//     else if(marks >= 80 && marks < 90){
-//         console.log(`${name} you have received A Grade`)
-//     }
-//     else if(marks >=70  && marks < 80 ){
-//         console.log(`${name} you have received B Grade`)
-//     }
-//      else if(marks >=60  && marks < 70){
-//         console.log(`${name} you have received C Grade`)
-//     } 
-//     else if(marks >= 50 && marks < 60){
-//         console.log(`${name} you have received D Grade`)
-//     }
-
-//     else if(marks >=40  && marks < 50){
-//         console.log(`${name} you have received E Grade`)
-//     }
-
-//     else if (marks >=0 && marks < 40){
-//         console.log(`Students has failed`)
-//     }
-
-//     else {
-//         console.log(`Invalid marks`)
-//     }
-// }
-
-// findGrand("Altaf", 88);
-// findGrand("Arbaj" , 77);
-//  findGrand("Sohel", 66);
-// findGrand("Altaf" , 55);
-// findGrand("Pathan" , 44);
-// findGrand("Patel",3);
-// findGrand("shan", 22);
-// ---------------------------------------------------------------------------------------------
-
-// function checkAge(age){
-//     if (age <= 35)
-//         console.log('Young Man')
-
-//     else{
-//         console.log('Old Man')
-//     }
-// }
-
-// checkAge(30);
-// ----------------------------------------------------------------------------------------------------
-
-// function variableMarks(marks){
-//     if (marks > 90){
-//         console.log(`AA`)
-//     }
-//     else if(marks >80 && marks <= 90){
-//         console.log(`AB`)
-//     }
-//     else if(marks > 70 && marks <= 80){
-//         console.log (`BB`)
-//     }
-//     else if (marks > 60 && marks <= 70){
-//         console.log(`BC`)
-//     }
-//     else if (marks > 50 && marks <= 60){
-//         console.log (`CC`)
-//     }
-//     else if (marks > 40 && marks <= 50){
-//         console.log (`CD`)
-//     }
-//     else if (marks > 30 && marks <= 40){
-//         console.log (`DD`)
-//     }
-//     else if (marks <= 40) {
-//         console.log (`FF`)
-//     }
-// }
-
-// variableMarks(92);
-// variableMarks(82);
-// variableMarks(72);
-// variableMarks(62);
-// variableMarks(52);
-// variableMarks(42);
-// variableMarks(32);
-// variableMarks(22);
-// variableMarks(92);
-// --------------------------------------------------------------------------------------------------------
-
-for (var i = 0; i < 3; i++){
-    let name = "Altaf patel";
-    console.log ("Hi, my name is " + name);
-}
-
-
-
-
-
-
-
-
+// var array1 = [5, 11, 20, 100];
+// console.log(array1.every((x) => x % 5 === 0));
+// -------------------------------------------------------------------------------------------------------------------------
---
+++ java.java
@@ -0,0 +1,209 @@
+// ----------------------------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------
+    // function isOddEvenNumber(num){
+    //     if (num % 2 ===0){
+    //         console.log (`${num}  is even number`) 
+    //     }
+    //     else {
+    //         console.log (`${num}  is odd number`) 
+
+    //     }
+    
+    // }
+
+    // // console.log( typeof(num));
+        
+
+    // isOddEvenNumber(12);
+    // isOddEvenNumber(13);
+
+// ------------------------------------------------------------------------------
+
+
+// function isValidNum(num){
+//     if(isNaN(num))
+//         console.log(`${num} variable is not a number`)
+
+//     else{
+//         console.log(`${num} Variable is a valid number`)
+//     }
+// }
+
+// // isValidNum('one');
+// // isValidNum(1);
+// isValidNum('45');
+// console.log(typeof('45'));
+// // isValidNum('char');
+// // isValidNum(45.5);
+// ------------------------------------------------------------------------------------------------
+
+// function findLargest(num1,num2){
+//     if(num1>num2){
+//         console.log (`${num1} is largest num1`)
+//     }
+//     else if (num1<num2){
+//         console.log (`${num2}  is largest num2`)
+
+//     }
+//     else{
+//         console.log(`${num1} is equal to ${num2}`)
+//     }
+// }
+
+// findLargest(10,20);
+// findLargest(20,20);
+// findLargest(35,20);
+// ------------------------------------------------------------------------------------------------------
+
+// function findLargest(num1, num2, num3){
+//     if(num1>num2 && num2>num3){
+//         console.log(`${num1} is greater largest than ${num2} ${num3}`)
+//     }
+//     else if(num2>num3){
+//         console.log(`${num2} is largest than ${num3}`)
+//     }
+//     else{
+//         console.log(`${num3} is largest number`)
+//     }
+// }
+
+
+
+// findLargest(35,10,2);
+// findLargest(10, 35,25);
+// findLargest(10,12,35);
+
+// -------------------------------------------------------------------------------------------------
+
+// function isTrigleEquilateralIsoscelesScalene(side1,side2,side3){
+//     if((side1==side2) && (side1==side3)){
+//         console.log(`Equilateral Trigle`);
+//     }
+
+//     else if(side1==side2 || side2==side3 || side1==side3){
+//         console.log(`isosceles Trigle`);
+//     }
+
+//     else{
+//         console.log(`Scalene Trigle`);
+//     }
+// }
+
+// isTrigleEquilateralIsoscelesScalene(10,10,10);
+// isTrigleEquilateralIsoscelesScalene(3,3,5);
+// isTrigleEquilateralIsoscelesScalene(20,21,22);
+// --------------------------------------------------------------------------------------------
+
+// function checkLearYear(year){
+//     if(((year % 4===0)&&(year % 100 !=0)) || (year % 400 ===0)){
+//         console.log (` ${year} leap year`);
+//     }
+//     else{
+//         console.log(` ${year} not a lear year`)
+//     }
+// }
+
+// checkLearYear(2012);
+// checkLearYear(2013);
+// checkLearYear(2014);
+// checkLearYear(2015);
+// checkLearYear(2016);
+// ----------------------------------------------------------------------------------------------
+
+// function findGrand(name,marks){
+
+//     if (marks >= 90 && marks <= 100)
+//         console.log(`${name} you have received S Grade`)
+    
+//     else if(marks >= 80 && marks < 90){
+//         console.log(`${name} you have received A Grade`)
+//     }
+//     else if(marks >=70  && marks < 80 ){
+//         console.log(`${name} you have received B Grade`)
+//     }
+//      else if(marks >=60  && marks < 70){
+//         console.log(`${name} you have received C Grade`)
+//     } 
+//     else if(marks >= 50 && marks < 60){
+//         console.log(`${name} you have received D Grade`)
+//     }
+
+//     else if(marks >=40  && marks < 50){
+//         console.log(`${name} you have received E Grade`)
+//     }
+
+//     else if (marks >=0 && marks < 40){
+//         console.log(`Students has failed`)
+//     }
+
+//     else {
+//         console.log(`Invalid marks`)
+//     }
+// }
+
+// findGrand("Altaf", 88);
+// findGrand("Arbaj" , 77);
+//  findGrand("Sohel", 66);
+// findGrand("Altaf" , 55);
+// findGrand("Pathan" , 44);
+// findGrand("Patel",3);
+// findGrand("shan", 22);
+// ---------------------------------------------------------------------------------------------
+
+// function checkAge(age){
+//     if (age <= 35)
+//         console.log('Young Man')
+
+//     else{
+//         console.log('Old Man')
+//     }
+// }
+
+// checkAge(30);
+// ----------------------------------------------------------------------------------------------------
+
+// function variableMarks(marks){
+//     if (marks > 90){
+//         console.log(`AA`)
+//     }
+//     else if(marks >80 && marks <= 90){
+//         console.log(`AB`)
+//     }
+//     else if(marks > 70 && marks <= 80){
+//         console.log (`BB`)
+//     }
+//     else if (marks > 60 && marks <= 70){
+//         console.log(`BC`)
+//     }
+//     else if (marks > 50 && marks <= 60){
+//         console.log (`CC`)
+//     }
+//     else if (marks > 40 && marks <= 50){
+//         console.log (`CD`)
+//     }
+//     else if (marks > 30 && marks <= 40){
+//         console.log (`DD`)
+//     }
+//     else if (marks <= 40) {
+//         console.log (`FF`)
+//     }
+// }
+
+// variableMarks(92);
+// variableMarks(82);
+// variableMarks(72);
+// variableMarks(62);
+// variableMarks(52);
+// variableMarks(42);
+// variableMarks(32);
+// variableMarks(22);
+// variableMarks(92);
+// --------------------------------------------------------------------------------------------------------
+
+// for ( i = 0; i < 10; i++){
+//     let name = "Hello world";
+//     console.log (name);
+//     // console.log(name);
+// }
